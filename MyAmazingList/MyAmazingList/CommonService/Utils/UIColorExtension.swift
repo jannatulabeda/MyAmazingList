097eca5d56df38f98614fb26757f30d0a1dad54b
Jannatul Abeda <jannatulabeda@Jannatuls-MacBook-Pro.local>
2019-08-24 14:26:42 +0900

    Initial commit

---
+++ MyAmazingList/MyAmazingList/CommonService/Utils/UIColorExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIColorExtension.swift
+//  MyAmazingList
+//
+//  Created by Jannatul Abeda on 2019/08/24.
+//  Copyright © 2019 Jannatul Abeda. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func _indicatorBackgroundColor() -> UIColor {
+        return UIColor.init(red: 209.0 / 255.0, green: 209.0 / 255.0, blue: 209.0 / 255.0, alpha: 1.0)
+    }
+}
---
+++ MyAmazingList/MyAmazingList/Features/DetailOnWebVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailOnWebVC.swift
+//  MyAmazingList
+//
+//  Created by Jannatul Abeda on 2019/08/24.
+//  Copyright © 2019 Jannatul Abeda. All rights reserved.
+//
+
+import UIKit
+
+class DetailOnWebVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
